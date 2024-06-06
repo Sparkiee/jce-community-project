9d228264c72902e4e2dd7999a36dfa2f6cb28a50
Maxwell <maxims970@gmail.com>
2024-06-06 08:37:16 +0300

    google png

--- crm-david-yelin/src/index.js
+++ crm-david-yelin/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Register from './regTest';
+import LoginMenu from './components/LoginMenu';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(

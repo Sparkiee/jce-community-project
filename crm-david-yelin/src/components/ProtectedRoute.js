e0868f9f7bcc9c0489bd2ebd7045cdbc5815ff1a
Maxwell <maxims970@gmail.com>
2024-06-09 18:17:44 +0300

    Changed internal site routing conditions
    kicks members to the home page when trying to access pages with invalid permissions

--- crm-david-yelin/src/components/ProtectedRoute.js
+++ crm-david-yelin/src/components/ProtectedRoute.js
@@ -3,8 +3,11 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, privilegeLevel }) => {
   const user = JSON.parse(sessionStorage.getItem("user"));
-  if (!user || user.privileges < privilegeLevel) {
+  if (!user || user.privileges === 0) {
     return <Navigate to="/" />;
+  }
+  if(user.privileges < privilegeLevel) {
+    return <Navigate to="/profile" />;
   }
   return children;
 };

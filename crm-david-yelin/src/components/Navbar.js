1f7f27183880dd379d4474ac017f679cf8ee10e0
Adam Shay <adam19255@gmail.com>
2024-06-10 20:58:28 +0300

    added CSS

--- crm-david-yelin/src/components/Navbar.js
+++ crm-david-yelin/src/components/Navbar.js
@@ -1,37 +1,51 @@
 import React from "react";
-import '../styles/Navbar.css';
+import "../styles/Navbar.css";
 
 function Navbar() {
-    return (
-        <header>
-            <button>התנתק</button>
-            <div className="logo">
-                <img src={require('../assets/profile.jpeg')} alt="logo" className="profile-logo" />
-            </div>
-            <nav>
-            <ul>
-                    <li>
-                        <a href="#">תקשורת</a>
-                        <ul className="dropdown">
-                            <li><a href="#">משהו</a></li>
-                            <li><a href="#">משהו</a></li>
-                            <li><a href="#">משהו</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">משימות</a>
-                        <ul className="dropdown">
-                            <li><a href="#">הוסף משימה</a></li>
-                            <li><a href="#">צפה במשימות</a></li>
-                            <li><a href="#">שנה משימה</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">ראשי</a></li>
-                </ul>
-            </nav>
-            <img src={require('../assets/lol.png')} alt="logo" className="brand-logo" />
-        </header>
-    );
+  return (
+    <div>
+      <button>התנתק</button>
+      <div className="logo">
+        <img src={require("../assets/profile.jpeg")} alt="logo" className="profile-logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">תקשורת</a>
+            <ul className="dropdown">
+              <li>
+                <a href="#">משהו</a>
+              </li>
+              <li>
+                <a href="#">משהו</a>
+              </li>
+              <li>
+                <a href="#">משהו</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">משימות</a>
+            <ul className="dropdown">
+              <li>
+                <a href="#">הוסף משימה</a>
+              </li>
+              <li>
+                <a href="#">צפה במשימות</a>
+              </li>
+              <li>
+                <a href="#">שנה משימה</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">ראשי</a>
+          </li>
+        </ul>
+      </nav>
+      <img src={require("../assets/lol.png")} alt="logo" className="brand-logo" />
+    </div>
+  );
 }
 
 export default Navbar;
--- crm-david-yelin/src/components/RegistrationForm.js
+++ crm-david-yelin/src/components/RegistrationForm.js
@@ -4,8 +4,8 @@ import { getDoc, doc, serverTimestamp, setDoc, deleteDoc } from "firebase/firest
 import { db } from "../firebase.js";
 import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
 import { auth } from "../firebase.js";
-
 import "../styles/RegistrationForm.css";
+import PhoneInput from "react-phone-number-input/input";
 
 const checkPendingRegistration = async (email) => {
   const docRef = doc(db, "awaiting_registration", email);
@@ -20,35 +20,30 @@ function RegistrationForm() {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [verifyPassword, setVerifyPassword] = useState("");
 
   const [pendingAccount, setPendingAccount] = useState(false);
   const [accountExists, setAccountExists] = useState(false);
 
   const navigate = useNavigate();
 
-  useEffect(() => {
-    let timer;
-    if (pendingAccount) {
-      timer = setTimeout(() => {
-        setPendingAccount(false);
-      }, 5000); // Change back to false after 5 seconds
-    }
-    return () => clearTimeout(timer); // This will clear the timeout if the component unmounts before the timeout finishes
-  }, [pendingAccount]);
+  // useEffect(() => {
+  //   let timer;
+  //   if (pendingAccount) {
+  //     timer = setTimeout(() => {
+  //       setPendingAccount(false);
+  //     }, 5000); // Change back to false after 5 seconds
+  //   }
+  //   return () => clearTimeout(timer); // This will clear the timeout if the component unmounts before the timeout finishes
+  // }, [pendingAccount]);
 
   async function handleSubmit(event) {
     event.preventDefault();
     try {
-      const isPendingRegistration = await checkPendingRegistration(
-        email.toLowerCase()
-      );
+      const isPendingRegistration = await checkPendingRegistration(email.toLowerCase());
       setPendingAccount(!isPendingRegistration);
       if (isPendingRegistration) {
-        const userCredential = await createUserWithEmailAndPassword(
-          auth,
-          email,
-          password
-        );
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
         console.log(`User registered with UID: ${user.uid}`);
         await sendEmailVerification(user);
@@ -65,24 +60,18 @@ function RegistrationForm() {
             phone: phone,
             privileges: 0,
             department: "General",
-            createdOn: serverTimestamp()
+            createdOn: serverTimestamp(),
           });
           console.log("Document successfully written!");
           setAccountExists(true);
 
           // Delete the document from the awaiting_registration collection
-          const awaitingRegistrationDocRef = doc(
-            db,
-            "awaiting_registration",
-            email
-          );
+          const awaitingRegistrationDocRef = doc(db, "awaiting_registration", email);
           await deleteDoc(awaitingRegistrationDocRef);
-          console.log(
-            "Document successfully deleted from awaiting_registration!"
-          );
+          console.log("Document successfully deleted from awaiting_registration!");
           // Navigate to login page
           setTimeout(() => {
-            navigate('/');
+            navigate("/");
           }, 5000);
         } catch (e) {
           console.error("Error writing document: ", e);
@@ -92,74 +81,77 @@ function RegistrationForm() {
     } catch (e) {}
     // Handle form submission
   }
+  const passwordsMatch = password === verifyPassword;
+
   return (
-    <div className="registration">
-      <h2 className="title">הרשמה</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="input-box">
-          <div className="first-name">
+    <div className="container">
+      <div className="registration">
+        <h2 className="title">הרשמה</h2>
+        <form className="registration-form" onSubmit={handleSubmit}>
+          <div className="registration-input-box">
             <input
               type="text"
               placeholder="שם פרטי"
-              className="styled-input"
+              className="registration-input"
               value={firstName}
               onChange={(event) => setFirstName(event.target.value)}
             />
-          </div>
-          <div className="last-name">
             <input
               type="text"
               placeholder="שם משפחה"
-              className="styled-input"
+              className="registration-input"
               value={lastName}
               onChange={(event) => setLastName(event.target.value)}
             />
-          </div>
-          <div className="id">
             <input
               type="text"
               placeholder="תעודת זהות"
-              className="styled-input"
+              className="registration-input"
               value={id}
               onChange={(event) => setId(event.target.value)}
             />
-          </div>
-          <div className="phone">
-            <input
-              type="tel"
+            <PhoneInput
+              defaultCountry="IL"
               placeholder="טלפון"
-              className="styled-input"
+              className="registration-input"
               value={phone}
-              onChange={(event) => setPhone(event.target.value)}
+              onChange={setPhone}
+              style={{ textAlign: "right" }}
             />
-          </div>
-          <div className="email">
             <input
               type="email"
               placeholder="אימייל"
-              className="styled-input"
+              className="registration-input"
               value={email}
               onChange={(event) => setEmail(event.target.value)}
             />
-          </div>
-          <div className="password">
             <input
               type="password"
               placeholder="סיסמה"
-              className="styled-input"
+              className="registration-input"
               value={password}
               onChange={(event) => setPassword(event.target.value)}
             />
+            <input
+              type="password"
+              placeholder="אמת סיסמה"
+              className="registration-input"
+              value={verifyPassword}
+              onChange={(event) => setVerifyPassword(event.target.value)}
+            />
           </div>
-        </div>
-        <button type="submit" className="styled-button">
-          הירשם
-        </button>
-        {pendingAccount && (
-          <p style={{ color: "red" }}>אימייל לא מורשה להרשם למערכת</p>
-        )}
-        {accountExists && <p style={{ color: "green" }}>משתמש נוצר, נא לאמת אימייל דרך התיבת דואר</p>}
-      </form>
+          <button type="submit" className="registration-form-button">
+            הירשם
+          </button>
+          <div className="feedback">
+            {pendingAccount && <p>אימייל לא מורשה להרשם למערכת</p>}
+            {accountExists && (
+              <p style={{ color: "green" }}>משתמש נוצר, נא לאמת אימייל דרך התיבת דואר</p>
+            )}
+            {!passwordsMatch && <p>הסיסמאות אינן תואמות</p>}
+          </div>
+        </form>
+      </div>
     </div>
   );
 }

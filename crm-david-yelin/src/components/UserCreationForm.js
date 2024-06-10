e8b8003567a320e16acccb24d2f8b056478b1da6
Adam Shay <adam19255@gmail.com>
2024-06-10 22:00:41 +0300

    added CSS and some functions

--- crm-david-yelin/src/components/UserCreationForm.js
+++ crm-david-yelin/src/components/UserCreationForm.js
@@ -1,68 +1,129 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { db } from "../firebase";
-import { getDoc, doc, serverTimestamp, setDoc} from "firebase/firestore";
+import { getDoc, doc, serverTimestamp, setDoc } from "firebase/firestore";
+import "../styles/UserCreationForm.css";
 
 const checkPendingRegistration = async (email) => {
-    const docRef = doc(db, "awaiting_registration", email);
-    const docSnap = await getDoc(docRef);
-    return docSnap.exists();
-}
+  const docRef = doc(db, "awaiting_registration", email);
+  const docSnap = await getDoc(docRef);
+  return docSnap.exists();
+};
 
 function UserCreationForm() {
-    const [email, setEmail] = useState('');
-    const [emailExists, setEmailExists] = useState(false);
-    const [accountCreated, setAccountCreated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailExists, setEmailExists] = useState(false);
+  const [accountCreated, setAccountCreated] = useState(false);
+  const [department, setDepartment] = useState("");
+  const [departmentList, setDepartmentList] = useState([]);
+  const [isOtherSelected, setIsOtherSelected] = useState(false);
+  const [newDepartment, setNewDepartment] = useState("");
 
-    useEffect(() => {
-        let timer;
-        if (accountCreated) {
-            timer = setTimeout(() => {
-                setAccountCreated(false);
-            }, 5000); // Change back to false after 5 seconds
-        }
-        return () => clearTimeout(timer); // This will clear the timeout if the component unmounts before the timeout finishes
-    }, [accountCreated]);
-
-    async function handleSubmit(event) {
-        event.preventDefault();
-        
-        try {
-            const isPendingRegistration = await checkPendingRegistration(email.toLowerCase());
-            if(isPendingRegistration) {
-                setEmailExists(true);
-                return;
-            }
-            else {
-                setEmailExists(false);
-                // Add email to pending registration
-                const docRef = doc(db, "awaiting_registration", email);
-                await setDoc(docRef, {
-                    email: email,
-                    timestamp: serverTimestamp()
-                });
-                setAccountCreated(true);
-            }
-            
-        } catch(e) {
-            console.error("Error adding document: ", e)
-        }
+  function addDepartment() {
+    if (newDepartment && !departmentList.includes(newDepartment)) {
+      setDepartmentList([...departmentList, newDepartment]);
+      setDepartment(newDepartment);
     }
-    return (
-      <div className="creation">
+    setIsOtherSelected(false);
+    setNewDepartment("");
+  }
+
+  useEffect(() => {
+    let timer;
+    if (accountCreated) {
+      timer = setTimeout(() => {
+        setAccountCreated(false);
+      }, 5000); // Change back to false after 5 seconds
+    }
+    return () => clearTimeout(timer); // This will clear the timeout if the component unmounts before the timeout finishes
+  }, [accountCreated]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const isPendingRegistration = await checkPendingRegistration(email.toLowerCase());
+      if (isPendingRegistration) {
+        setEmailExists(true);
+        return;
+      } else {
+        setEmailExists(false);
+        // Add email to pending registration
+        const docRef = doc(db, "awaiting_registration", email);
+        await setDoc(docRef, {
+          email: email,
+          timestamp: serverTimestamp(),
+        });
+        setAccountCreated(true);
+      }
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+  return (
+    <div className="container">
+      <div className="create-user">
         <h2 className="title">יצירת משתמש</h2>
         <form onSubmit={handleSubmit}>
-          <div className="input-box">
-            <div className="email">
-              <input type="email" value={email} onChange={(event)=> setEmail(event.target.value)} placeholder="אימייל" className="styled-input"/>
+          <div className="create-user-form">
+            <div className="create-user-input-box">
+              <input
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="אימייל"
+                className="create-user-input"
+              />
+              <select
+                name="department"
+                value={department}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  if (value === "other") {
+                    setIsOtherSelected(true);
+                    setDepartment("");
+                  } else {
+                    setIsOtherSelected(false);
+                    setDepartment(value);
+                  }
+                }}
+                className="create-user-input">
+                <option value="" disabled>
+                  בחר מחלקה
+                </option>
+                {departmentList.map((dept, index) => (
+                  <option key={index} value={dept}>
+                    {dept}
+                  </option>
+                ))}
+                <option value="other">הוסף מחלקה</option>
+              </select>
+              {isOtherSelected && (
+                <div className="new-department">
+                  <input
+                    type="text"
+                    value={newDepartment}
+                    placeholder="הוסף מחלקה"
+                    onChange={(event) => setNewDepartment(event.target.value)}
+                    className="create-user-input"
+                  />
+                  <button type="button" onClick={addDepartment} className="create-button">
+                    הוסף
+                  </button>
+                </div>
+              )}
+            </div>
+            <button type="submit" className="create-button">
+              יצירה
+            </button>
+            <div className="feedback">
+              {emailExists && <p>אימייל כבר קיים במערכת</p>}
+              {accountCreated && <p style={{ color: "green" }}>המשתמש נוצר בהצלחה</p>}
             </div>
           </div>
-          <button type="submit" className="styled-button">יצירה</button>
-          {emailExists && <p style={{color: 'red'}}>אימייל כבר קיים במערכת</p>}
-          {accountCreated && <p style={{color: 'green'}}>המשתמש נוצר בהצלחה</p>}
-          
         </form>
       </div>
-    );
-  }
-  
-  export default UserCreationForm;
+    </div>
+  );
+}
+
+export default UserCreationForm;

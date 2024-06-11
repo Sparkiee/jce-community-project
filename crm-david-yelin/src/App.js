c27889335408106e655a8af31a4775594aac747a
Maxwell <maxims970@gmail.com>
2024-06-11 17:16:58 +0300

    fixed imports

--- crm-david-yelin/src/App.js
+++ crm-david-yelin/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import LoginForm from './components/LoginForm';
 import HomePage from './components/HomePage';
+import UserCreationForm from './components/UserCreationForm';
+import RegistrationForm from './components/RegistrationForm';
+import CreateEvent from './components/CreateEvent';
 
 const App = () => {
   return (

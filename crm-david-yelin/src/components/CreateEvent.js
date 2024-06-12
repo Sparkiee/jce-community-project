96f6a2adefe9bd8aeea12e63f630b7e73aa04880
Maxim Shteingard <maxims970@gmail.com>
2024-06-12 12:30:27 +0300

    fixed createEvent to list selected members, added remove functionality and base for notifications per user (#6)

--- crm-david-yelin/src/components/CreateEvent.js
+++ crm-david-yelin/src/components/CreateEvent.js
@@ -7,7 +7,10 @@ import {
   where,
   addDoc,
   doc,
-  serverTimestamp
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  arrayUnion
 } from "firebase/firestore";
 import "../styles/CreateEvent.css";
 import Select from "react-select";
@@ -19,10 +22,47 @@ function CreateEvent() {
   const [eventDetails, setEventDetails] = useState({
     eventName: "",
     eventDate: "",
+    eventTime: "",
     eventLocation: "",
     assignees: selectedMembers
   });
 
+  // async function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const assigneeRefs = selectedMembers.map((member) =>
+  //     doc(db, "members", member.id)
+  //   );
+  //   const updatedEventDetails = {
+  //     eventName: eventDetails.eventName,
+  //     eventDate: eventDetails.eventDate,
+  //     eventLocation: eventDetails.eventLocation,
+  //     eventCreated: serverTimestamp(),
+  //     assignees: assigneeRefs,
+  //     eventStatus: "בתהליך"
+  //   };
+  //   try {
+  //     const docRef = await addDoc(
+  //       collection(db, "events"),
+  //       updatedEventDetails
+  //     );
+  //     console.log("Event recorded with ID: ", docRef.id);
+  //     await Promise.all(selectedMembers.map(async (member) => {
+  //       const memberRef = doc(db, "members", member.id);
+  //       const notification = {
+  //         eventID: docRef.id,
+  //         notificationTime: serverTimestamp(),
+  //         notificationMessage: "הינך משובץ לאירוע חדש!"
+  //       };
+  
+  //       // Assuming Notifications is an array, use arrayUnion to add a new notification
+  //       await updateDoc(memberRef, {
+  //         Notifications: arrayUnion(notification)
+  //       });
+  //     }));
+  //   } catch (e) {
+  //     console.error("Error adding document: ", e);
+  //   }
+  // }
   async function handleSubmit(event) {
     event.preventDefault();
     const assigneeRefs = selectedMembers.map((member) =>
@@ -31,24 +71,37 @@ function CreateEvent() {
     const updatedEventDetails = {
       eventName: eventDetails.eventName,
       eventDate: eventDetails.eventDate,
+      eventTime: eventDetails.eventTime,
+      eventLocation: eventDetails.eventLocation,
       eventCreated: serverTimestamp(),
       assignees: assigneeRefs,
       eventStatus: "בתהליך"
     };
+  
     try {
-      const docRef = await addDoc(
-        collection(db, "events"),
-        updatedEventDetails
-      );
+      const docRef = await addDoc(collection(db, "events"), updatedEventDetails);
       console.log("Event recorded with ID: ", docRef.id);
+  
+      // Use forEach for side effects
+      await Promise.all(selectedMembers.map(async (member) => {
+        const memberRef = doc(db, "members", member.id);
+  
+        // Set the timestamp separately
+        await updateDoc(memberRef, {
+          Notifications: arrayUnion({
+            eventID: docRef,
+            notificationMessage: `הינך משובץ לאירוע חדש ${eventDetails.eventName}!`,
+          })
+        });
+      }));
+  
+      console.log("Notifications updated for all members.");
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   }
 
   async function handleSearch(event) {
-    console.log("searching!");
-    setSearch(event.target.value);
     if (event.target.value.length >= 2) {
       const membersRef = collection(db, "members");
       const q = query(
@@ -115,6 +168,11 @@ function CreateEvent() {
               }
             />
             <input
+            type="time"
+            name="eventTime"
+            className="create-event-input"
+            onChange={(e) => setEventDetails({ ...eventDetails, eventTime: e.target.value})} />
+            <input
               type="text"
               placeholder="מיקום האירוע"
               name="eventLocation"
@@ -139,50 +197,52 @@ function CreateEvent() {
               placeholder="הוסף חבר וועדה"
               className="create-event-input"
               onInputChange={(inputValue) => {
-                console.log("Input changed:", inputValue);
+                handleSearch({ target: { value: inputValue } });
               }}
               onChange={(e) => {
-                console.log("triggered")
-                // console.log("Selected:", selectedOption);
-                // handleSelectMember(selectedOption);
+                handleSelectMember(e.value);
               }}
               options={members.map((member) => ({
                 value: member.fullName,
                 label: member.fullName
               }))}
             />
-            <input
-              list="members"
-              placeholder="חפש חבר וועדה להוספה"
-              value={search}
-              className="create-event-input"
-              onChange={(e) => {
-                handleSearch(e);
-                handleSelectMember(e.target.value);
-              }}
-            />
-            <datalist id="members">
-              {members.map((member, index) => (
-                <option key={index} value={member.fullName} />
+            <div className="create-event-selected-members">
+              {selectedMembers.map((member, index) => (
+                <div
+                  key={index}
+                  className="selected-member"
+                  onClick={() => handleRemoveMember(member.id)}
+                >
+                  {/* <i className="icon-class-name">👤</i> */}
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M16 12h4m-2 2v-4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                  <div className="selected-member-text">{member.fullName}</div>
+                </div>
               ))}
-            </datalist>
+            </div>
           </div>
-
-          <br />
           <input
             type="submit"
             value="שלח"
             className="primary-button extra-create-event"
           />
         </form>
-        <h2>Selected Members:</h2>
-        <ul>
-          {selectedMembers.map((member) => (
-            <li key={member.id} onClick={() => handleRemoveMember(member.id)}>
-              {member.fullName}
-            </li>
-          ))}
-        </ul>
       </div>
     </div>
   );

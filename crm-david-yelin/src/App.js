2424427b7fc8775b3674115001ee879a4abd1e12
Adam Shay <adam19255@gmail.com>
2024-06-20 15:24:49 +0300

    added tasks page

--- crm-david-yelin/src/App.js
+++ crm-david-yelin/src/App.js
@@ -1,30 +1,45 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
-import LoginForm from './components/LoginForm';
-import HomePage from './components/HomePage';
-import CreateUser from './components/CreateUser';
-import RegisterUser from './components/RegisterUser';
-import CreateEvent from './components/CreateEvent';
-import CreateTask from './components/CreateTask';
-import ManageUsers from './components/ManageUsers';
-
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginForm from "./components/LoginForm";
+import HomePage from "./components/HomePage";
+import CreateUser from "./components/CreateUser";
+import RegisterUser from "./components/RegisterUser";
+import CreateEvent from "./components/CreateEvent";
+import CreateTask from "./components/CreateTask";
+import ManageUsers from "./components/ManageUsers";
+import ManageTasks from "./components/ManageTasks";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<LoginForm/>} />
-        <Route path="/home" element={<ProtectedRoute privilegeLevel={1} ><HomePage/></ProtectedRoute>} />
-        <Route path="/create" element={<ProtectedRoute privilegeLevel={3} ><CreateUser/></ProtectedRoute>} />
-        <Route path="/register" element={<RegisterUser/>} />
-        <Route path="/create-event" element={<CreateEvent/>} />
-        <Route path="/create-task" element={<CreateTask/>} />
-        <Route path="/manage" element={<ManageUsers/>} />
+        <Route exact path="/" element={<LoginForm />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute privilegeLevel={1}>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create"
+          element={
+            <ProtectedRoute privilegeLevel={3}>
+              <CreateUser />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/create-task" element={<CreateTask />} />
+        <Route path="/manage" element={<ManageUsers />} />
+        <Route path="/manage-task" element={<ManageTasks />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;

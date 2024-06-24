a7c32edbe8dc18347b83de102f1217225b7ad3c6
Adam Shay <adam19255@gmail.com>
2024-06-24 19:27:02 +0300

    changed confirm

--- crm-david-yelin/src/components/ConfirmAction.js
+++ crm-david-yelin/src/components/ConfirmAction.js
@@ -1,28 +1,32 @@
 import "../styles/Styles.css";
 import "../styles/ConfirmAction.css";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 
-import React, { forwardRef, useImperativeHandle } from 'react';
+import React, { forwardRef, useImperativeHandle } from "react";
 
 const ConfirmAction = forwardRef(({ onConfirm, onCancel }, ref) => {
-    // Use useImperativeHandle to expose specific functions to the parent component
-    useImperativeHandle(ref, () => ({
-      triggerConfirmClick() {
-        onConfirm();
-      },
-      triggerCancelClick() {
-        onCancel();
-      },
-    }));
-  
-    return (
-        <div className="confirm-action-form">
-           <h2 className="confirm-title">האם אתה בטוח שברצונך לבצע פעולה זו?</h2>
-            <div className="confirm-action-buttons">
-                <input type="submit" value="אשר" className="confirm-button" onClick={() => onConfirm()} />
-                <input type="submit" value="בטל" className="deny-button" onClick={() => onCancel()} />
-        </div>
+  // Use useImperativeHandle to expose specific functions to the parent component
+  useImperativeHandle(ref, () => ({
+    triggerConfirmClick() {
+      onConfirm();
+    },
+    triggerCancelClick() {
+      onCancel();
+    },
+  }));
+
+  return (
+    <div className="confirm-action-form">
+      <div className="alert-name">
+        <WarningAmberIcon className="Warning-Amber-Icon" />
+        <h2 className="confirm-actions-title">האם אתה בטוח שברצונך לבצע פעולה זו?</h2>
       </div>
-    );
-  });
-  
-  export default ConfirmAction;
+      <div className="confirm-action-buttons">
+        <input type="submit" value="אשר" className="confirm-button" onClick={() => onConfirm()} />
+        <input type="submit" value="בטל" className="deny-button" onClick={() => onCancel()} />
+      </div>
+    </div>
+  );
+});
+
+export default ConfirmAction;

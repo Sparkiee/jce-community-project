f74040e12953a0d62f05f6275a3af7adb0510da8
Adam Shay <adam19255@gmail.com>
2024-07-06 16:22:30 +0300

    CSS fixes

--- crm-david-yelin/src/components/EditDepartment.js
+++ crm-david-yelin/src/components/EditDepartment.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../styles/EditDepartment.css";
 
 function EditDepartment(props) {
   const [department, setDepartment] = useState(props.department.name || "");
@@ -7,22 +8,43 @@ function EditDepartment(props) {
     e.preventDefault();
     props.onComplete(department);
   }
-  
+
   return (
-    <div className="change-password-form">
+    <div className="edit-department-style">
       <div
         className="action-close"
         onClick={() => {
           props.onClose();
         }}>
-        <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-          <line x1="17" y1="7" x2="7" y2="17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-          <line x1="7" y1="7" x2="17" y2="17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+        <svg
+          width="24px"
+          height="24px"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor">
+          <line
+            x1="17"
+            y1="7"
+            x2="7"
+            y2="17"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+          <line
+            x1="7"
+            y1="7"
+            x2="17"
+            y2="17"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
         </svg>
       </div>
-      <form onSubmit={handleSubmit}>
-        <h1 className="change-password-title">עדכון מחלקה</h1>
-        <div className="change-password-input-container">
+      <form className="edit-department-form" onSubmit={handleSubmit}>
+        <h1 className="edit-department-title">עדכון מחלקה</h1>
+        <div className="edit-department-input-container">
           <input
             type="input"
             placeholder="שם מחלקה"

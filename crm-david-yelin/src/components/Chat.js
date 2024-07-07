68b3935647c65c2b6b1b40d1b1820b1081ddc0c5
Adam Shay <adam19255@gmail.com>
2024-07-07 10:41:49 +0300

    well... too much to count...

--- crm-david-yelin/src/components/Chat.js
+++ crm-david-yelin/src/components/Chat.js
@@ -1,10 +1,14 @@
 import React, { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import EmojiPicker from "emoji-picker-react";
 import "../styles/Chat.css";
 import InfoIcon from "@mui/icons-material/Info";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import ImageIcon from "@mui/icons-material/Image";
 import Avatar from "@mui/material/Avatar";
+import Badge from "@mui/material/Badge";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import { db } from "../firebase";
 import {
   getDoc,
@@ -20,7 +24,6 @@ import {
   updateDoc,
   addDoc,
 } from "firebase/firestore";
-import { useNavigate } from "react-router-dom";
 
 function Chat() {
   function stringToColor(string) {
@@ -64,6 +67,8 @@ function Chat() {
   const [selectedChat, setSelectedChat] = useState(null);
   const [messages, setMessages] = useState([]);
   const [user, setUser] = useState(null);
+  const [isChatsLoading, setIsChatsLoading] = useState(true);
+  const [isSearchLoading, setIsSearchLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -91,6 +96,7 @@ function Chat() {
 
   const fetchUserChats = async () => {
     if (!user) return;
+    setIsChatsLoading(true);
     try {
       const chatRef = collection(db, "chats");
       const q = query(chatRef, where("members", "array-contains", user.email));
@@ -100,11 +106,16 @@ function Chat() {
         let otherUserEmail = doc.data().members.find((email) => email !== user.email);
         const fullName = await getMemberFullName(otherUserEmail); // Now it's valid to use await here
         const chat = doc.data();
-        chatArray.push({ chatId: doc.id, otherUserEmail, fullName, ...chat });
+        const unseenCount = chat.messages.filter(
+          (message) => message.sender !== user.email && !message.seen
+        ).length;
+        chatArray.push({ chatId: doc.id, otherUserEmail, fullName, unseenCount, ...chat });
       }
       setChats(chatArray);
     } catch (error) {
       console.error("Failed to fetch user chats:", error);
+    } finally {
+      setIsChatsLoading(false);
     }
   };
 
@@ -127,6 +138,22 @@ function Chat() {
     setEmojiMode(false);
   };
 
+  useEffect(() => {
+    if (user) {
+      const chatRef = collection(db, "chats");
+      const q = query(chatRef, where("members", "array-contains", user.email));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        querySnapshot.docChanges().forEach((change) => {
+          if (change.type === "modified") {
+            fetchUserChats();
+          }
+        });
+      });
+
+      return () => unsubscribe();
+    }
+  }, [user]);
+
   const handleSearch = async (e) => {
     e.preventDefault();
     const searchValue = e.target.username.value;
@@ -135,7 +162,7 @@ function Chat() {
       setSearchResults([]);
       return;
     }
-
+    setIsSearchLoading(true);
     try {
       const q = query(
         collection(db, "members"),
@@ -148,6 +175,8 @@ function Chat() {
     } catch (error) {
       console.error("Error searching for users:", error);
       setSearchResults([]);
+    } finally {
+      setIsSearchLoading(false);
     }
   };
 
@@ -191,6 +220,18 @@ function Chat() {
     }
   };
 
+  const handleChatSelection = async (chat) => {
+    setSelectedChat(chat);
+    if (chat.unseenCount > 0) {
+      const chatRef = doc(db, "chats", chat.chatId);
+      const updatedMessages = chat.messages.map((message) =>
+        message.sender !== user.email && !message.seen ? { ...message, seen: true } : message
+      );
+      await updateDoc(chatRef, { messages: updatedMessages });
+      fetchUserChats();
+    }
+  };
+
   const handleSendMessage = async () => {
     if (!text || !selectedChat) return;
     const chatRef = doc(db, "chats", selectedChat.chatId);
@@ -222,7 +263,7 @@ function Chat() {
   };
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && !e.shiftKey && text.trim() !== "") {
       handleSendMessage();
     }
   };
@@ -245,85 +286,116 @@ function Chat() {
             </div>
           </div>
           <div className="chat-list">
-            <div className="chat-list-search">
-              <div className="chat-list-search-bar">
-                <svg
-                  viewBox="0 0 32 32"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#000000">
-                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                  <g id="SVGRepo_iconCarrier">
-                    <title>search</title>
-                    <desc>Created with Sketch Beta.</desc>
-                    <defs></defs>
-                    <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            {isChatsLoading ? (
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100%",
+                }}>
+                <CircularProgress />
+              </Box>
+            ) : (
+              <>
+                <div className="chat-list-search">
+                  <div className="chat-list-search-bar">
+                    <svg
+                      viewBox="0 0 32 32"
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#000000">
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                       <g
-                        id="Icon-Set"
-                        transform="translate(-256.000000, -1139.000000)"
-                        fill="#000000">
-                        <path
-                          d="M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z"
-                          id="search"></path>
-                      </g>
-                    </g>
-                  </g>
-                </svg>
-                <input type="text" placeholder="חפש משתמש..." />
-              </div>
-              <div className="chat-list-search-add-minus">
-                {!addMode ? (
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    onClick={() => setAddMode(true)}>
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                    <g id="SVGRepo_iconCarrier">
-                      <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#ffffff"></path>
-                    </g>
-                  </svg>
-                ) : (
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    onClick={() => setAddMode(false)}>
-                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                    <g id="SVGRepo_iconCarrier">
-                      {" "}
-                      <path
-                        d="M6 12L18 12"
-                        stroke="#ffffff"
-                        strokeWidth="2"
+                        id="SVGRepo_tracerCarrier"
                         strokeLinecap="round"
-                        strokeLinejoin="round"></path>{" "}
-                    </g>
-                  </svg>
-                )}
-              </div>
-            </div>
-            {chats.map((chat, index) => (
-              <div
-                className={`chat-list-item ${
-                  selectedChat && selectedChat.chatId === chat.chatId ? "selected" : ""
-                }`}
-                key={index}
-                onClick={() => setSelectedChat(chat)}>
-                <Avatar
-                  {...stringAvatar(chat && chat.fullName)}
-                  title={chat.fullName || "Unknown Name"}
-                  className="chat-list-item-avatar"
-                />
-                <div className="chat-list-item-texts">
-                  <span>{chat.fullName}</span>
-                  <p>{chat.lastMessage}</p>
+                        strokeLinejoin="round"></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <title>search</title>
+                        <desc>Created with Sketch Beta.</desc>
+                        <defs></defs>
+                        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                          <g
+                            id="Icon-Set"
+                            transform="translate(-256.000000, -1139.000000)"
+                            fill="#000000">
+                            <path
+                              d="M269.46,1163.45 C263.17,1163.45 258.071,1158.44 258.071,1152.25 C258.071,1146.06 263.17,1141.04 269.46,1141.04 C275.75,1141.04 280.85,1146.06 280.85,1152.25 C280.85,1158.44 275.75,1163.45 269.46,1163.45 L269.46,1163.45 Z M287.688,1169.25 L279.429,1161.12 C281.591,1158.77 282.92,1155.67 282.92,1152.25 C282.92,1144.93 276.894,1139 269.46,1139 C262.026,1139 256,1144.93 256,1152.25 C256,1159.56 262.026,1165.49 269.46,1165.49 C272.672,1165.49 275.618,1164.38 277.932,1162.53 L286.224,1170.69 C286.629,1171.09 287.284,1171.09 287.688,1170.69 C288.093,1170.3 288.093,1169.65 287.688,1169.25 L287.688,1169.25 Z"
+                              id="search"></path>
+                          </g>
+                        </g>
+                      </g>
+                    </svg>
+                    <input type="text" placeholder="חפש משתמש..." />
+                  </div>
+                  <div className="chat-list-search-add-minus">
+                    {!addMode ? (
+                      <svg
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        onClick={() => setAddMode(true)}>
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g
+                          id="SVGRepo_tracerCarrier"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                          <path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z" fill="#ffffff"></path>
+                        </g>
+                      </svg>
+                    ) : (
+                      <svg
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        onClick={() => setAddMode(false)}>
+                        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                        <g
+                          id="SVGRepo_tracerCarrier"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                          <path
+                            d="M6 12L18 12"
+                            stroke="#ffffff"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"></path>
+                        </g>
+                      </svg>
+                    )}
+                  </div>
                 </div>
-              </div>
-            ))}
+                {chats.map((chat, index) => (
+                  <div
+                    className={`chat-list-item ${
+                      selectedChat && selectedChat.chatId === chat.chatId ? "selected" : ""
+                    }`}
+                    key={index}
+                    onClick={() => handleChatSelection(chat)}>
+                    <div className="chat-list-item-right-side">
+                      <Avatar
+                        {...stringAvatar(chat && chat.fullName)}
+                        title={chat.fullName || "Unknown Name"}
+                        className="chat-list-item-avatar"
+                      />
+                      <div className="chat-list-item-texts">
+                        <span>{chat.fullName}</span>
+                        <p>{chat.lastMessage}</p>
+                      </div>
+                    </div>
+                    {chat.unseenCount > 0 && (
+                      <Badge
+                        badgeContent={chat.unseenCount}
+                        color="primary"
+                        className="chat-list-item-badge"
+                      />
+                    )}
+                  </div>
+                ))}
+              </>
+            )}
           </div>
         </div>
         {selectedChat && (
@@ -364,7 +436,7 @@ function Chat() {
                       }`}>
                       <div className="chat-messages-center-message-texts">
                         {message.img && <img src={message.img} />}
-                        <p>{message.text}</p>
+                        <pre>{message.text}</pre>
                         <span>
                           {new Date(
                             message.timestamp.seconds * 1000 +
@@ -380,7 +452,7 @@ function Chat() {
                 <div className="chat-messages-bottom-icons">
                   <ImageIcon />
                 </div>
-                <input
+                <textarea
                   className="chat-messages-bottom-input"
                   type="text"
                   placeholder="שלח הודעה..."
@@ -410,11 +482,19 @@ function Chat() {
           </form>
           {searchQuery && (
             <div className="chat-user">
-              {searchResults && searchResults.length > 0 ? (
+              {isSearchLoading ? (
+                <Box sx={{ display: "flex", justifyContent: "center", padding: "20px" }}>
+                  <CircularProgress />
+                </Box>
+              ) : searchResults && searchResults.length > 0 ? (
                 searchResults.map((user) => (
                   <div className="chat-user-list" key={user.email}>
                     <div className="chat-user-detail">
-                      <img src={require("../assets/profile.jpg")} alt="profile" />
+                      <Avatar
+                        {...stringAvatar(user.fullName || "Unknown Name")}
+                        title={user.fullName || "Unknown Name"}
+                        className="chat-user-avatar"
+                      />
                       <h2>{user.fullName}</h2>
                     </div>
                     <button onClick={() => handleAddUser(user)}>הוסף</button>

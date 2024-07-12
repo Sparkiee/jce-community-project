2b88c784a6fb0ee0d49cb766c9649a8c38e5b32e
Maxwell <maxims970@gmail.com>
2024-07-12 17:53:40 +0300

    fixed privileges bug

--- crm-david-yelin/src/components/ProtectedRoute.js
+++ crm-david-yelin/src/components/ProtectedRoute.js
@@ -3,14 +3,22 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, privilegeLevel, extraAccess }) => {
   let user = JSON.parse(sessionStorage.getItem("user"));
-  if(!user) {
+  if (!user) {
     user = JSON.parse(localStorage.getItem("user"));
   }
   if (!user || user.privileges === 0) {
     return <Navigate to="/" />;
   }
-  if(user.privileges < privilegeLevel || extraAccess && (Array.isArray(user.adminAccess) && !user.adminAccess.some((access) => extraAccess.includes(access)))){
+  if (user.privileges < privilegeLevel) {
     return <Navigate to="/home" />;
+  }
+  if (Array.isArray(user.adminAccess) && Array.isArray(extraAccess)) {
+    const hasAccess = (user.adminAccess.some((access) => extraAccess.includes(access)) || user.privileges >= 2);
+
+    if (!hasAccess) {
+      // Redirect to home if no access rights match
+      return <Navigate to="/home" />;
+    }
   }
   return children;
 };

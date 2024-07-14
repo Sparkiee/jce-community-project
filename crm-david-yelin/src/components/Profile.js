3f87c84f342c894bde9129786bff0466487c2892
Maxwell <maxims970@gmail.com>
2024-07-14 15:12:36 +0300

    hotfix to last

--- crm-david-yelin/src/components/Profile.js
+++ crm-david-yelin/src/components/Profile.js
@@ -188,7 +188,7 @@ function Profile() {
       align: "right",
       flex: 3,
     },
-    ...(user && (user.privileges >= 2 || (Array.isArray(user.adminAccess) && user.adminAccess.includes("viewBudget"))) ? [
+    ...(user && (user.privileges >= 2 || (Array.isArray(user.adminAccess) && user.adminAccess.includes("viewBudget")) || profile.email === user.email) ? [
     {
       field: "taskBudget",
       headerName: "תקציב",
@@ -383,7 +383,7 @@ function Profile() {
       align: "right",
       flex: 3,
     },
-    ...(user && (user.privileges >= 2 || (Array.isArray(user.adminAccess) && user.adminAccess.includes("viewBudget"))) ? [
+    ...(user && (user.privileges >= 2 || (Array.isArray(user.adminAccess) && user.adminAccess.includes("viewBudget")) || user.email === profile.email) ? [
     {
       field: "eventBudget",
       headerName: "תקציב",
